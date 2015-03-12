c02afb20de8e02888b4d4fdc8a9978461e054de1
mrgreen991 <greenwork-school@hotmail.com>
2015-03-12 10:11:11 +0100

    noget

--- BackUp/BackUp/Options.Designer.cs
+++ BackUp/BackUp/Options.Designer.cs
@@ -36,21 +36,21 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.openBD = new System.Windows.Forms.FolderBrowserDialog();
             this.groupBackUpWhy = new System.Windows.Forms.GroupBox();
-            this.rbTime = new System.Windows.Forms.RadioButton();
-            this.rbUpdateInRegion = new System.Windows.Forms.RadioButton();
+            this.rbUdateManual = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMins = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.rbUpdateWhenTime = new System.Windows.Forms.RadioButton();
+            this.rbComputerClosing = new System.Windows.Forms.RadioButton();
             this.txtMin = new System.Windows.Forms.TextBox();
+            this.rbUpdateInRegion = new System.Windows.Forms.RadioButton();
+            this.rbTime = new System.Windows.Forms.RadioButton();
             this.barAmounts = new System.Windows.Forms.TrackBar();
             this.lblBackups = new System.Windows.Forms.Label();
             this.groupCompression = new System.Windows.Forms.GroupBox();
-            this.rbComNone = new System.Windows.Forms.RadioButton();
-            this.rbComZip = new System.Windows.Forms.RadioButton();
             this.rbComRar = new System.Windows.Forms.RadioButton();
-            this.rbComputerClosing = new System.Windows.Forms.RadioButton();
-            this.rbUpdateWhenTime = new System.Windows.Forms.RadioButton();
-            this.txtHours = new System.Windows.Forms.TextBox();
-            this.txtMins = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.rbUdateManual = new System.Windows.Forms.RadioButton();
+            this.rbComZip = new System.Windows.Forms.RadioButton();
+            this.rbComNone = new System.Windows.Forms.RadioButton();
             this.chStart = new System.Windows.Forms.CheckBox();
             this.groupBackUpWhy.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.barAmounts)).BeginInit();
@@ -109,18 +109,75 @@
             this.groupBackUpWhy.Size = new System.Drawing.Size(761, 100);
             this.groupBackUpWhy.TabIndex = 4;
             this.groupBackUpWhy.TabStop = false;
-            this.groupBackUpWhy.Text = "BackUp Why:";
+            this.groupBackUpWhy.Text = "BackUp When:";
             // 
-            // rbTime
+            // rbUdateManual
             // 
-            this.rbTime.AutoSize = true;
-            this.rbTime.Location = new System.Drawing.Point(230, 23);
-            this.rbTime.Name = "rbTime";
-            this.rbTime.Size = new System.Drawing.Size(144, 21);
-            this.rbTime.TabIndex = 0;
-            this.rbTime.Text = "Update every min:";
-            this.rbTime.UseVisualStyleBackColor = true;
-            this.rbTime.CheckedChanged += new System.EventHandler(this.rbTime_CheckedChanged);
+            this.rbUdateManual.AutoSize = true;
+            this.rbUdateManual.Location = new System.Drawing.Point(230, 52);
+            this.rbUdateManual.Name = "rbUdateManual";
+            this.rbUdateManual.Size = new System.Drawing.Size(155, 21);
+            this.rbUdateManual.TabIndex = 8;
+            this.rbUdateManual.Text = "Update manual only";
+            this.rbUdateManual.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = ":";
+            // 
+            // txtMins
+            // 
+            this.txtMins.Enabled = false;
+            this.txtMins.Location = new System.Drawing.Point(184, 50);
+            this.txtMins.Name = "txtMins";
+            this.txtMins.Size = new System.Drawing.Size(24, 22);
+            this.txtMins.TabIndex = 6;
+            this.txtMins.Text = "00";
+            // 
+            // txtHours
+            // 
+            this.txtHours.Enabled = false;
+            this.txtHours.Location = new System.Drawing.Point(136, 50);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(24, 22);
+            this.txtHours.TabIndex = 5;
+            this.txtHours.Text = "12";
+            // 
+            // rbUpdateWhenTime
+            // 
+            this.rbUpdateWhenTime.AutoSize = true;
+            this.rbUpdateWhenTime.Location = new System.Drawing.Point(6, 50);
+            this.rbUpdateWhenTime.Name = "rbUpdateWhenTime";
+            this.rbUpdateWhenTime.Size = new System.Drawing.Size(129, 21);
+            this.rbUpdateWhenTime.TabIndex = 4;
+            this.rbUpdateWhenTime.Text = "Update on time:";
+            this.rbUpdateWhenTime.UseVisualStyleBackColor = true;
+            this.rbUpdateWhenTime.CheckedChanged += new System.EventHandler(this.rbUpdateWhenTime_CheckedChanged);
+            // 
+            // rbComputerClosing
+            // 
+            this.rbComputerClosing.AutoSize = true;
+            this.rbComputerClosing.Location = new System.Drawing.Point(458, 24);
+            this.rbComputerClosing.Name = "rbComputerClosing";
+            this.rbComputerClosing.Size = new System.Drawing.Size(223, 21);
+            this.rbComputerClosing.TabIndex = 3;
+            this.rbComputerClosing.Text = "Update when computer closing";
+            this.rbComputerClosing.UseVisualStyleBackColor = true;
+            // 
+            // txtMin
+            // 
+            this.txtMin.Enabled = false;
+            this.txtMin.Location = new System.Drawing.Point(380, 23);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(72, 22);
+            this.txtMin.TabIndex = 2;
+            this.txtMin.Text = "120";
+            this.txtMin.TextChanged += new System.EventHandler(this.txtMin_TextChanged);
             // 
             // rbUpdateInRegion
             // 
@@ -134,15 +191,16 @@
             this.rbUpdateInRegion.Text = "Update when change in folder";
             this.rbUpdateInRegion.UseVisualStyleBackColor = true;
             // 
-            // txtMin
+            // rbTime
             // 
-            this.txtMin.Enabled = false;
-            this.txtMin.Location = new System.Drawing.Point(380, 23);
-            this.txtMin.Name = "txtMin";
-            this.txtMin.Size = new System.Drawing.Size(72, 22);
-            this.txtMin.TabIndex = 2;
-            this.txtMin.Text = "120";
-            this.txtMin.TextChanged += new System.EventHandler(this.txtMin_TextChanged);
+            this.rbTime.AutoSize = true;
+            this.rbTime.Location = new System.Drawing.Point(230, 23);
+            this.rbTime.Name = "rbTime";
+            this.rbTime.Size = new System.Drawing.Size(144, 21);
+            this.rbTime.TabIndex = 0;
+            this.rbTime.Text = "Update every min:";
+            this.rbTime.UseVisualStyleBackColor = true;
+            this.rbTime.CheckedChanged += new System.EventHandler(this.rbTime_CheckedChanged);
             // 
             // barAmounts
             // 
@@ -174,27 +232,6 @@
             this.groupCompression.TabStop = false;
             this.groupCompression.Text = "Compression Type";
             // 
-            // rbComNone
-            // 
-            this.rbComNone.AutoSize = true;
-            this.rbComNone.Checked = true;
-            this.rbComNone.Location = new System.Drawing.Point(6, 21);
-            this.rbComNone.Name = "rbComNone";
-            this.rbComNone.Size = new System.Drawing.Size(63, 21);
-            this.rbComNone.TabIndex = 3;
-            this.rbComNone.Text = "None";
-            this.rbComNone.UseVisualStyleBackColor = true;
-            // 
-            // rbComZip
-            // 
-            this.rbComZip.AutoSize = true;
-            this.rbComZip.Location = new System.Drawing.Point(75, 21);
-            this.rbComZip.Name = "rbComZip";
-            this.rbComZip.Size = new System.Drawing.Size(51, 21);
-            this.rbComZip.TabIndex = 4;
-            this.rbComZip.Text = ".zip";
-            this.rbComZip.UseVisualStyleBackColor = true;
-            // 
             // rbComRar
             // 
             this.rbComRar.AutoSize = true;
@@ -206,63 +243,27 @@
             this.rbComRar.Text = ".rar";
             this.rbComRar.UseVisualStyleBackColor = true;
             // 
-            // rbComputerClosing
+            // rbComZip
             // 
-            this.rbComputerClosing.AutoSize = true;
-            this.rbComputerClosing.Location = new System.Drawing.Point(458, 24);
-            this.rbComputerClosing.Name = "rbComputerClosing";
-            this.rbComputerClosing.Size = new System.Drawing.Size(223, 21);
-            this.rbComputerClosing.TabIndex = 3;
-            this.rbComputerClosing.Text = "Update when computer closing";
-            this.rbComputerClosing.UseVisualStyleBackColor = true;
+            this.rbComZip.AutoSize = true;
+            this.rbComZip.Location = new System.Drawing.Point(75, 21);
+            this.rbComZip.Name = "rbComZip";
+            this.rbComZip.Size = new System.Drawing.Size(51, 21);
+            this.rbComZip.TabIndex = 4;
+            this.rbComZip.Text = ".zip";
+            this.rbComZip.UseVisualStyleBackColor = true;
             // 
-            // rbUpdateWhenTime
+            // rbComNone
             // 
-            this.rbUpdateWhenTime.AutoSize = true;
-            this.rbUpdateWhenTime.Location = new System.Drawing.Point(6, 50);
-            this.rbUpdateWhenTime.Name = "rbUpdateWhenTime";
-            this.rbUpdateWhenTime.Size = new System.Drawing.Size(129, 21);
-            this.rbUpdateWhenTime.TabIndex = 4;
-            this.rbUpdateWhenTime.Text = "Update on time:";
-            this.rbUpdateWhenTime.UseVisualStyleBackColor = true;
-            this.rbUpdateWhenTime.CheckedChanged += new System.EventHandler(this.rbUpdateWhenTime_CheckedChanged);
-            // 
-            // txtHours
-            // 
-            this.txtHours.Enabled = false;
-            this.txtHours.Location = new System.Drawing.Point(136, 50);
-            this.txtHours.Name = "txtHours";
-            this.txtHours.Size = new System.Drawing.Size(24, 22);
-            this.txtHours.TabIndex = 5;
-            this.txtHours.Text = "12";
-            // 
-            // txtMins
-            // 
-            this.txtMins.Enabled = false;
-            this.txtMins.Location = new System.Drawing.Point(184, 50);
-            this.txtMins.Name = "txtMins";
-            this.txtMins.Size = new System.Drawing.Size(24, 22);
-            this.txtMins.TabIndex = 6;
-            this.txtMins.Text = "00";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(166, 52);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(12, 17);
-            this.label1.TabIndex = 7;
-            this.label1.Text = ":";
-            // 
-            // rbUdateManual
-            // 
-            this.rbUdateManual.AutoSize = true;
-            this.rbUdateManual.Location = new System.Drawing.Point(230, 52);
-            this.rbUdateManual.Name = "rbUdateManual";
-            this.rbUdateManual.Size = new System.Drawing.Size(155, 21);
-            this.rbUdateManual.TabIndex = 8;
-            this.rbUdateManual.Text = "Update manual only";
-            this.rbUdateManual.UseVisualStyleBackColor = true;
+            this.rbComNone.AutoSize = true;
+            this.rbComNone.Checked = true;
+            this.rbComNone.Location = new System.Drawing.Point(6, 21);
+            this.rbComNone.Name = "rbComNone";
+            this.rbComNone.Size = new System.Drawing.Size(63, 21);
+            this.rbComNone.TabIndex = 3;
+            this.rbComNone.TabStop = true;
+            this.rbComNone.Text = "None";
+            this.rbComNone.UseVisualStyleBackColor = true;
             // 
             // chStart
             // 
